c3eb561edb752e9b538695ef5bd752b0e462f000
gitcjh <1418503647@.qq.com>
2017-11-22 22:21:06 +0800

    测试修改

--- src/js/index.js
+++ src/js/index.js
@@ -166,12 +166,15 @@ initMap()
 $('#loading').hide()
 
 // var url = encodeURIComponent(location.href.split('#')[0])
-var url = location.href.split('#')[0]
-let domain = 'http://m.liangchuantech.com'
+
 // let domain = 'http://192.168.3.22:12222'
 // let domain = 'http://120.24.226.112:12222'
+
+var url = location.href.split('#')[0]
+let domain = 'http://m.liangchuantech.com'
+
 $.ajax({
-    url: domain + "/wechat/jsapi?url=" + url,
+    url: domain + "/wechat/jsapi?url=" + encodeURIComponent(url),
     success: function (result) {
         console.log('原先的URL')
         console.log(location.href.split('#')[0])
